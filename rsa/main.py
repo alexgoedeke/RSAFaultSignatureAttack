ebec9404324d317da69e72343b929445c6f5861a
Alexander Gödeke <goedeke.alexander@hotmail.de>
2016-02-10 15:15:20 +0100

    initial-commit

--- rsa/main.py
+++ rsa/main.py
@@ -2,9 +2,37 @@
 # created on 10.02.2016 by Alexander Goedeke
 
 from random import randint
+import os
 
 # use test values for RSA
-p, q, n, e, d = 593, 487, 288791, 5, 115085
+#p, q, n, e, d = 593, 487, 288791, 5, 115085
+p, q, n, e, d = 0, 0, 0, 0, 0
+
+# read private key file (a=1: generate new openssl rsa key)
+def generateKey(a):
+    if a==1:
+        os.system("openssl genrsa -out private.key")
+        os.system("openssl rsa -in private.key -text > private.txt")
+
+    global p, q, n, e, d
+
+    f = open('private.txt', 'r')
+    text = f.read()
+
+    i_n = text.find("modulus")
+    i_e = text.find("publicExponent")
+    i_d = text.find("privateExponent")
+    i_p = text.find("prime1")
+    i_q = text.find("prime2")
+    i_end = text.find("exponent1")
+
+    n = int(text[ i_n + 7: i_e].translate(None, ': \n'), 16)
+    e = int(text[ i_e + 16: i_e+21])
+    d = int(text[ i_d + 15: i_p].translate(None, ': \n'), 16)
+    p = int(text[ i_p + 6: i_q].translate(None, ': \n'), 16)
+    q = int(text[ i_q + 6: i_end].translate(None, ': \n'), 16)
+
+    f.close()
 
 
 # greatest common divisor of a and b (euclidean algorithm)
@@ -50,6 +78,7 @@ def crt(a1, a2):
 def sig(h):
     return crt(h, h)
 
+
 # bitflip
 def bitflip(h):
     i = "{0:b}".format(h)
@@ -68,8 +97,13 @@ def fsig(h):
 
 
 def main():
-    # read plaintext
+    generateKey(0)
+
+    # read plaintext dec
     h = int(input("plaintext: "))
+
+    # read plaintext hex
+    # h = int(input("plaintext: "), 16)
 
     # generate signatures
     s1 = sig(h)
@@ -83,7 +117,11 @@ def main():
     # we know that s1 is an correct and s2 an incorrect signature
     p1, p2, p3 = gcd(n, s1 - s2)
     if p1 == p:
-        print "found private key: (" + str(p1) + ", " + str(n / p1) + ")"
+        print "found private key (1): (" + str(p1) + ", " + str(n / p1) + ")"
+
+    p1, p2, p3 = gcd(n, power(s2, e, n) - h)
+    if p1 == p:
+        print "found private key (2): (" + str(p1) + ", " + str(n / p1) + ")"
 
     return 0
 
